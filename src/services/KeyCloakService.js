8b1de06ed6e1aa5079b870b46b8e39af29d48d36
Daniel Herzig <daniel.herzig@outlook.at>
2023-06-12 11:23:25 +0200

    use initial init value.

--- src/services/KeyCloakService.js
+++ src/services/KeyCloakService.js
@@ -42,7 +42,7 @@ class KeyCloakService extends AbstractService{
             const keycloak = new Keycloak(this.initOptions);
             keycloak.init({
 		onLoad: 'check-sso',
-		silentCheckSsoRedirectUri: window.location.origin + '/silent-check-sso.html'
+		silentCheckSsoRedirectUri: window.location.origin + '/sso.html'
             })
           
             keycloak.onReady = () => {
