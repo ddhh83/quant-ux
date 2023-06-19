727596c365b3c7a1e617f7282d1872a780236843
Daniel Herzig <daniel.herzig@outlook.at>
2023-06-19 21:00:33 +0200

    try to fix undefined.json.

--- src/services/KeyCloakService.js
+++ src/services/KeyCloakService.js
@@ -55,11 +55,11 @@ class KeyCloakService extends AbstractService{
           keycloak.loadUserProfile().then(async user => {
             this.logger.log(-1, 'init()', 'user loaded', user)
             const quxUser = {
-              id: user.id,
-              name: user.username,
-              lastname: user.lastname,
-              email: user.username,
-              token: keycloak.token
+              id: keycloak.profile.id,
+              name: keycloak.profile.userName,
+              lastname: keycloak.profile.lastName,
+              email: keycloak.profile.email,
+              token: keycloak.idToken
             }
             this.isInited = true
             this.setUser(quxUser)
@@ -71,11 +71,11 @@ class KeyCloakService extends AbstractService{
                 Logger.error('Keycloak failed to refresh token')
               })
               const quxUser = {
-                id: user.id,
-                name: user.username,
-                lastname: user.lastname,
-                email: user.username,
-                token: keycloak.token
+                id: keycloak.profile.id,
+                name: keycloak.profile.username,
+                lastname: keycloak.profile.lastName,
+                email: keycloak.profile.email,
+                token: keycloak.idToken
               }
               this.setUser(quxUser)
             }, this.REFRESH_INTERVAl)
